f9805a002ddbdce7b011992e00b6430442171916
EleanaPol <45208539+EleanaPol@users.noreply.github.com>
2022-01-28 17:12:21 +0000

    ca game of life

---
+++ CA/Assets/Scripts/CA_cell.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CA_cell
+{
+    public int ID;
+    public Vector2 normalized_coords;
+    public Vector2 position;
+}
