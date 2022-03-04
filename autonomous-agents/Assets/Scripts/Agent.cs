64327af503db532918a708eb021134b684b61d9f
EleanaPol <45208539+EleanaPol@users.noreply.github.com>
2022-03-04 17:15:29 +0000

    commit before changing branch

--- autonomous-agents/Assets/Scripts/Agent.cs
+++ autonomous-agents/Assets/Scripts/Agent.cs
@@ -7,20 +7,42 @@ public class Agent : MonoBehaviour
     public int id;
     public Vector3 position;
     public Vector3 velocity;
+    public Vector3 last_position;
     public PopulationManager population;
-    
-    [Header("Speeds")]
+
+    [Header("Speeds")] 
+    public float global_speed;
     public float speed;
     public float attraction_speed;
+    // ------- flocking speeds -----
     public float separation_speed;
+    public float cohesion_speed;
+    public float alignment_speed;
 
     [Header("Weights")] 
     public float randomness_weight;
     public float attraction_weight;
+    // ------- flocking weights -----
     public float separation_weight;
+    public float cohesion_weight;
+    public float alignment_weight;
 
+    // ------- flocking distance thresholds -----
     [Header("Flocking")] 
     public float separation_threshold;
+    public float cohesion_threshold;
+    public float alignment_threshold;
+    
+    // ------- MOVEMENT FORCES ------
+    private Vector3 random_force;
+    private Vector3 attraction_force;
+    // ------- flocking ---------
+    private Vector3 separation_force;
+    private Vector3 cohesion_force;
+    private Vector3 alignment_force;
+    
+    
+    
     
     // Start is called before the first frame update
     void Start()
@@ -43,7 +65,7 @@ public class Agent : MonoBehaviour
         Vector3 new_position = new Vector3(x,0,z);
         Vector3 normalized_new_position = Vector3.Normalize(new_position);
         
-        return  normalized_new_position * speed ;
+        return  normalized_new_position * speed * global_speed ;
     }
 
     public Vector3 Attract()
@@ -51,7 +73,7 @@ public class Agent : MonoBehaviour
         var attractor_pos = population.attractor.position;
         var attraction_vector = attractor_pos - position;
         //var normalized_attraction = Vector3.Normalize(attraction_vector);
-        return attraction_vector * attraction_speed;
+        return attraction_vector * attraction_speed * global_speed;
     }
 
     public Vector3 Separate()
@@ -81,9 +103,67 @@ public class Agent : MonoBehaviour
         }
         else
         {
-            return (separation / separation_counter) * separation_speed;
+            return (separation / separation_counter) * separation_speed *global_speed;
         }
 
+    }
+
+    public Vector3 Cohesion()
+    {
+        Vector3 cohesion = Vector3.zero;
+        int cohesion_counter = 0;
+        
+        int numAgents = population.numAgents;
+        var agentPopulation = population.agentPopulation;
+
+        for (int i = 0; i < numAgents; i++)
+        {
+            Vector3 diff = agentPopulation[i].position - position;
+            float dist = diff.magnitude;
+
+            if (dist > 0 && dist <= cohesion_threshold)
+            {
+                //diff = Vector3.Normalize(diff);
+                cohesion += diff;
+                cohesion_counter++;
+            }
+        }
+
+        if (cohesion_counter > 0)
+        {
+            cohesion = (cohesion / cohesion_counter) * cohesion_speed *global_speed;
+        }
+        
+        return cohesion;
+    }
+
+    public Vector3 Align()
+    {
+        Vector3 alignment = Vector3.zero;
+        int alignment_counter = 0;
+        
+        int numAgents = population.numAgents;
+        var agentPopulation = population.agentPopulation;
+
+        for (int i = 0; i < numAgents; i++)
+        {
+            Vector3 diff = position - agentPopulation[i].position;
+            float dist = diff.magnitude;
+
+            if (dist > 0 && dist <= alignment_threshold)
+            {
+                Vector3 vel = agentPopulation[i].velocity;
+                alignment += vel;
+                alignment_counter++;
+            }
+        }
+
+        if (alignment_counter > 0)
+        {
+            alignment = (alignment / alignment_counter) * alignment_speed *global_speed;
+        }
+
+        return alignment;
     }
 
     public void AddForce(Vector3 force, float weight)
@@ -91,34 +171,61 @@ public class Agent : MonoBehaviour
         transform.position += force * weight * Time.deltaTime;
     }
 
+    public void CalculateForces()
+    {
+        random_force = RandomMove();
+        attraction_force = Attract();
+        
+        // ---- flocking forces
+        separation_force = Separate();
+        cohesion_force = Cohesion();
+        alignment_force = Align();
+    }
+
     public void MoveAgent()
     {
         // move randomly
-        Vector3 random = RandomMove();
-        AddForce(random,randomness_weight);
-        
+        AddForce(random_force,randomness_weight);
         // attract
-        Vector3 attraction = Attract();
-        AddForce(attraction,attraction_weight);
+        AddForce(attraction_force,attraction_weight);
         
-        //separate
-        Vector3 separation = Separate();
-        AddForce(separation,separation_weight);
+        // ----- flocking
+        AddForce(separation_force,separation_weight);
+        AddForce(cohesion_force,cohesion_weight);
+        AddForce(alignment_force,alignment_weight);
         
         // make sure to update position property
         position = transform.position;
+        
+        UpdateVelocity();
+    }
+
+    public void UpdateVelocity()
+    {
+        velocity = position - last_position;
+        last_position = position;
     }
 
     public void UpdateValues()
     {
+        global_speed = population.global_speed;
         speed = population.speed;
         attraction_speed = population.attraction_speed;
+        // ---- flocking speeds
         separation_speed = population.separation_speed;
+        cohesion_speed = population.cohesion_speed;
+        alignment_speed = population.alignment_speed;
 
         randomness_weight = population.randomness_weight;
         attraction_weight = population.attraction_weight;
+        // ----- flocking weights
         separation_weight = population.separation_weight;
+        cohesion_weight = population.cohesion_weight;
+        alignment_weight = population.alignment_weight;
 
+        // ----- flocking thresholds
         separation_threshold = population.separation_threshold;
+        cohesion_threshold = population.cohesion_threshold;
+        alignment_threshold = population.alignment_threshold;
     }
 }
--- autonomous-agents/Assets/Scripts/PopulationManager.cs
+++ autonomous-agents/Assets/Scripts/PopulationManager.cs
@@ -10,23 +10,35 @@ public class PopulationManager : MonoBehaviour
     public Vector2 x_range;
     public Vector2 z_range;
     public List<Agent> agentPopulation;
-    
-    [Header("Agent Settings")]
+
+    [Header("Agent Settings")] 
     [Header("Speeds")]
+    public float global_speed;
     public float speed;
     public float attraction_speed;
+    // -------- flocking speeds
     public float separation_speed;
+    public float cohesion_speed;
+    public float alignment_speed;
 
     [Header("Weights")] 
     public float randomness_weight;
     public float attraction_weight;
+    // -------- flocking weights
     public float separation_weight;
+    public float cohesion_weight;
+    public float alignment_weight;
 
     [Header("External Elements")] 
     public Transform attractor;
     
-    [Header("Flocking")] 
+    [Header("Flocking Thresholds")] 
     public float separation_threshold;
+    public float cohesion_threshold;
+    public float alignment_threshold;
+
+    public Material trail_mat;
+    public Gradient trail_color;
     
     // Start is called before the first frame update
     void Start()
@@ -37,10 +49,18 @@ public class PopulationManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // first calculate agent forces
         for (int i = 0; i < numAgents; i++)
         {
             var agent = agentPopulation[i];
             agent.UpdateValues();
+            agent.CalculateForces();
+        }
+
+        // then move agent
+        for (int i = 0; i < numAgents; i++)
+        {
+            var agent = agentPopulation[i];
             agent.MoveAgent();
         }
     }
@@ -56,7 +76,13 @@ public class PopulationManager : MonoBehaviour
             // random position for each agent
             float x = Random.Range(x_range.x, x_range.y);
             float z = Random.Range(z_range.x, z_range.y);
+
+            float xv = Random.Range(-x_range.y, x_range.y);
+            float zv = Random.Range(-z_range.y, z_range.y);
+            
             Vector3 new_pos = new Vector3(x,0,z);
+            Vector3 new_velocity = new Vector3(xv,0,zv);
+            
             new_agent.transform.position = new_pos;
             new_agent.name = "agent_" + i.ToString();
             
@@ -66,18 +92,41 @@ public class PopulationManager : MonoBehaviour
             agentPopulation.Add(agent_f);
             agent_f.id = i;
             agent_f.position = new_pos;
+            agent_f.last_position = new_pos;
+            agent_f.velocity = new_velocity;
             agent_f.population = this;
 
+            // SPEED
+            agent_f.global_speed = global_speed;
             agent_f.speed = speed;
             agent_f.attraction_speed = attraction_speed;
+            // ----- flocking speeds
             agent_f.separation_speed = separation_speed;
+            agent_f.cohesion_speed = cohesion_speed;
+            agent_f.alignment_speed = alignment_speed;
 
+            // WEIGHT
             agent_f.randomness_weight = randomness_weight;
             agent_f.attraction_weight = attraction_weight;
+            // ----- flocking weights
             agent_f.separation_weight = separation_weight;
+            agent_f.cohesion_weight = cohesion_weight;
+            agent_f.alignment_weight = alignment_weight;
 
+            // DISTANCE THRESHOLDS
+            // ----- flocking thresholds
             agent_f.separation_threshold = separation_threshold;
+            agent_f.cohesion_threshold = cohesion_threshold;
+            agent_f.alignment_threshold = alignment_threshold;
 
+
+            new_agent.AddComponent<TrailRenderer>();
+            var trail = new_agent.GetComponent<TrailRenderer>();
+            trail.startWidth = 0.05f;
+            trail.endWidth = 0.05f;
+            trail.time = 35;
+            trail.material = trail_mat;
+            trail.colorGradient = trail_color;
         }
     }
 }
